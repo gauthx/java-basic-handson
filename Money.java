85d2057d439532a1696c0abcce9f216a5074dc05
Gautham Krishna <gauthamkrishna@Gauthams-MacBook-Pro.local>
2026-04-27 15:25:19 +0530

    implements the specified classes

--- Money.java
+++ Money.java
@@ -1,1 +1,17 @@
 // Money.java
+
+class Money{
+  final int value;
+  Money(int initial){
+    this.value = initial;
+  }
+
+  Money add(Money Toadd){
+    return new Money(this.value + Toadd.value);
+  }
+
+  Money minus(Money ToSubtract){
+     return new Money(this.value - ToSubtract.value);
+  }
+  
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,33 @@
 // StringUtils
+class StringUtils{
+   static boolean containsChar(String string, char target){
+    for(int i=0 ; i < string.length(); i++){
+      if(string.charAt(i) == target){
+        return true;
+      }
+    }
+
+    return false;
+   }
+
+   static String reverse(String string){
+    String reversed = "";
+    for(int i=string.length() - 1 ; i >= 0; i--){
+      reversed = reversed + string.charAt(i);
+    }
+
+    return reversed;
+   }
+
+   static String replaceAll(String string, char target, char replacement){
+    String replaced = "";
+    for(int i=0 ; i < string.length(); i++){
+      char resultChar = string.charAt(i) == target ? replacement : string.charAt(i);
+      replaced += resultChar;
+    }
+
+    return replaced;
+
+   }
+
+}
